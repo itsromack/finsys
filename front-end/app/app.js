bc71b69777b9ffcf8f289001e601bb80a7ca4bc9
unknown <eallanjoseph@yahoo.com>
2017-07-10 21:08:30 +1200

    AJE - initial codes for front end

---
+++ front-end/app/app.js
@@ -0,0 +1,29 @@
+(function () {
+    "use strict";
+        angular.module('finsys-app',
+        [ 'finsys-app.router'
+        ]).
+        
+        
+        config(['$qProvider', function ($qProvider) {
+            $qProvider.errorOnUnhandledRejections(false);
+        }])
+        .controller('mainController',mainController);
+        
+        mainController.$inject = ['$window','$scope','$log'];
+        
+        function mainController(window,scope,log){
+        	var vm = this;
+        	
+        	var year = new Date().getFullYear();
+        	vm.modalApp = {
+        		header:"Car App",
+        		footer:"All rights reserved "+ year
+        	};
+        	window.baseUrl = window.location.href;
+        	
+        	log.info("main controller ");
+        	
+        }
+
+})();
