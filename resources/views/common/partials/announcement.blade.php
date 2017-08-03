7c8abb456531dac1e01bf6032a2e482b936e61b3
Romack Natividad <romacknatividad@gmail.com>
2017-08-03 11:28:47 +0800

    Removed laravel files

--- resources/views/common/partials/announcement.blade.php
+++
@@ -1,9 +0,0 @@
-@if (Session::has('announcement'))
-<div class="ui success message">
-  <i class="close icon"></i>
-  <div class="header">
-    <i class="icon announcement"></i>
-    {{ Session::get('announcement')}}
-  </div>
-</div>
-@endif
